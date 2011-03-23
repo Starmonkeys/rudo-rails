83f08da8eb97e7c130b46bb7fd24c74609542e25
Robert Fletcher <lobatifricha@gmail.com>
2011-03-22 19:50:23 -0700

    adding user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110223032202) do
+ActiveRecord::Schema.define(:version => 20110323022328) do
 
   create_table "tasks", :force => true do |t|
     t.string   "title"
     t.datetime "done_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
